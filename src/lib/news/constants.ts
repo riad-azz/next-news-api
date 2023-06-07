59788df1d24a058113bd55d85bac469a2063c027
Riadh <riad.azz.mail@gmail.com>
2023-06-07 23:52:02 +0100

    Update Added more news RSS sources.

--- src/lib/news/constants.ts
+++ src/lib/news/constants.ts
@@ -1,47 +1,122 @@
 export const newsSources: Source[] = [
   {
-    short: "NYT",
-    name: "New York Times",
-    url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
-  },
-  {
-    short: "CNN",
-    name: "CNN News",
-    url: "http://rss.cnn.com/rss/cnn_topstories.rss",
-  },
-  {
-    short: "HUFFPOST",
-    name: "Huffington Post",
-    url: "https://www.huffpost.com/section/front-page/feed",
-  },
-  {
-    short: "FOXNEWS",
-    name: "Fox News",
-    url: "https://moxie.foxnews.com/google-publisher/latest.xml",
-  },
-  {
-    short: "LIFEHACKER",
-    name: "Life Hacker",
-    url: "https://lifehacker.com/rss",
-  },
-  {
-    short: "REUTERS",
-    name: "Reuters",
-    url: "https://cdn.feedcontrol.net/8/1114-wioSIX3uu8MEj.xml",
-  },
-  {
-    short: "POLITICO",
-    name: "Politico",
-    url: "http://www.politico.com/rss/politicopicks.xml",
-  },
-  {
-    short: "YAHOO",
+    short: "INTER-YN",
     name: "Yahoo News",
     url: "https://www.yahoo.com/news/rss",
   },
   {
-    short: "LATIMES",
+    short: "INTER-LH",
+    name: "Life Hacker",
+    url: "https://lifehacker.com/rss",
+  },
+  {
+    short: "US-NYT",
+    name: "New York Times",
+    url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+  },
+  {
+    short: "US-CNN",
+    name: "CNN News",
+    url: "http://rss.cnn.com/rss/cnn_topstories.rss",
+  },
+  {
+    short: "US-HP",
+    name: "Huffington Post",
+    url: "https://www.huffpost.com/section/front-page/feed",
+  },
+  {
+    short: "US-FN",
+    name: "Fox News",
+    url: "https://moxie.foxnews.com/google-publisher/latest.xml",
+  },
+  {
+    short: "US-R",
+    name: "Reuters",
+    url: "https://cdn.feedcontrol.net/8/1114-wioSIX3uu8MEj.xml",
+  },
+  {
+    short: "US-P",
+    name: "Politico",
+    url: "http://www.politico.com/rss/politicopicks.xml",
+  },
+  {
+    short: "US-LAT",
     name: "Los Angeles Times",
     url: "https://www.latimes.com/local/rss2.0.xml",
+  },
+  {
+    short: "AU-DT",
+    name: "Latest News and News Headlines - Daily Telegraph",
+    url: "https://www.dailytelegraph.com.au/news/breaking-news/rss",
+  },
+  {
+    short: "AU-SMH",
+    name: "Sydney Morning Herald - Latest News",
+    url: "Sydney Morning Herald - Latest News",
+  },
+  {
+    short: "AU-HS",
+    name: "Latest News and News Headlines - Herald Sun",
+    url: "https://www.heraldsun.com.au/news/breaking-news/rss",
+  },
+  {
+    short: "AU-ABC",
+    name: "ABC News",
+    url: "https://www.abc.net.au/news/feed/1948/rss.xml",
+  },
+  {
+    short: "AU-TA",
+    name: "The Age - Latest News",
+    url: "https://www.theage.com.au/rss/feed.xml",
+  },
+  {
+    short: "AU-CM",
+    name: "The Courier Mail",
+    url: "https://www.couriermail.com.au/rss",
+  },
+  {
+    short: "AU-PN",
+    name: "PerthNow",
+    url: "https://www.perthnow.com.au/news/feed",
+  },
+  {
+    short: "AU-CT",
+    name: "The Canberra Times - Local News",
+    url: "https://www.canberratimes.com.au/rss.xml",
+  },
+  {
+    short: "AU-BT",
+    name: "Brisbane Times - Latest News",
+    url: "https://www.brisbanetimes.com.au/rss/feed.xml",
+  },
+  {
+    short: "AU-IA",
+    name: "Independent Australia",
+    url: "http://feeds.feedburner.com/IndependentAustralia",
+  },
+  {
+    short: "AU-BN",
+    name: "Business News - Latest Headlines",
+    url: "https://www.businessnews.com.au/rssfeed/latest.rss",
+  },
+  {
+    short: "AU-ID",
+    name: "InDaily",
+    url: "https://indaily.com.au/feed/",
+  },
+  {
+    short: "AU-TM",
+    name: "The Mercury",
+    url: "https://www.themercury.com.au/rss",
+  },
+  {
+    short: "AU-C",
+    name: "Crikey",
+    url: "https://feeds.feedburner.com/com/rCTl",
+  },
+  {
+    short: "AU-MW",
+    name: "Michael West",
+    url: "https://www.michaelwest.com.au/feed/",
   },
 ];
